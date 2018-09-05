ff6d08acf3ec572e4293d214c88306098b3ab42d
Prithvi Thakur <prithvi.thakur93@gmail.com>
2018-09-05 11:39:32 -0400

    working on main

--- main.py
+++ main.py
@@ -19,22 +19,59 @@ import pandas as pd                 # to read csv files
 
 # Change default plot settings
 plt.rcParams['font.size'] = 16
-plt.rcParams['axes.labelsize'] = 14
-plt.rcParams['font.titlesize'] = 16
-plt.rcParams['xtick.labelsize'] = 14
-plt.rcParams['ytick.labelsize'] = 14
-plt.rcParams['legend.fontsize'] = 16
+#  plt.rcParams['axes.labelsize'] = 14
+#  plt.rcParams['font.titlesize'] = 16
+#  plt.rcParams['xtick.labelsize'] = 14
+#  plt.rcParams['ytick.labelsize'] = 14
+#  plt.rcParams['legend.fontsize'] = 16
 
 # Read data from the downloaded CSV file
-# Return the latitude and longitude
-class input_data: 
-    data = pd.read_csv('data.html')
+class input_data:
+
+    data = pd.read_csv("data.html", skiprows=13)
     latitude = np.array(data["Latitude"])
     longitude = np.array(data["Longitude"])
     DateTime = np.array(data["DateTime"])
     Mw = np.array(data["Magnitude"])
 
-    def time_series(self):
-        # Change the time (yyyy-mm-dd-h-m-s) to continuous format
-        
-        return Mw, time_
+#  Change the time (yyyy-mm-dd-h-m-s) to continuous format
+def continuous_time(DateTime):
+   
+    # pre allocate
+    time_ = np.zeros(len(DateTime))
+    i = 0
+    for el in DateTime:
+        y = float(el[0:4])
+        m = float(el[5:7])
+        d = float(el[8:10])
+        h = float(el[11:13])
+        min = float(el[14:16])
+        s = float(el[17:22])
+
+        time_[i] = y + (m + (d + (h + (min + s/60)/60)/24)/30)/12 
+        i = i+1
+
+    return time_
+
+#  return Mw, time_
+inp = input_data()
+tt = continuous_time(inp.DateTime)
+print(max(tt))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
